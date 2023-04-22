43787e5a14a6b99f92590c3dffc53c24aa2edae9
tomastraini <tomas.traini@institutozonaoeste.edu.ar>
2023-04-22 20:45:09 -0300

    Added cors

--- router.js
+++ router.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var router = express.Router({mergeParams: true});
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
 //////////////////
 const app = express();
 const port = process.env.PORT || 1002;
